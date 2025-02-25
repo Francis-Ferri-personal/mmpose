0a10f596866c6e870b23b7386b118575fda54f83
Francis Ferri <francisferrir@gmail.com>
2025-02-25 13:42:19 -0800

    Updated DRAC traning script

--- tools/drac-train.sh
+++ tools/drac-train.sh
@@ -21,19 +21,21 @@ tar -xv --use-compress-program="pigz -d -p 4" -f $projects/Datasets/pigpose.tar.
 
 
 # VIRTUAL ENVIRONMENT SETUP
-module purge
-module load gcc cuda/12.6 arrow opencv/4.10.0 python/3.10
+module load scipy-stack gcc cuda/12.6 arrow opencv/4.10.0 python/3.10
 virtualenv --no-download $SLURM_TMPDIR/.venv
 source $SLURM_TMPDIR/.venv/bin/activate
 pip install --upgrade pip
 
 
 # INSTALL DEPENDENCIES
-pip install -r drac-requirements.txt
+# pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu126
+pip install --no-binary torch==2.1.2 torchvision==0.16.2
+pip install -U openmim
 mim install mmengine
 mim install "mmcv==2.1.0"
 mim install "mmdet>=3.1.0"
 pip install -r requirements.txt
+python setup.py install
 pip install -v -e .
 
 
@@ -63,13 +65,14 @@ tar -cvf $projects/Outputs/pigpose/${EXPERIMENT_NAME}-${SEED}.tar work_dirs/${EX
 
 # RUN YOUR JOB
 # cd $project/Workspace/mmpose
-# sbatch tools\drac-train.sh configs/animal_2d_keypoint/topdown_heatmap/pigpose/td-hm_hrnet-w32_8xb64-210e_pigpose-256x256.py 2 42
+# sbatch tools/drac-train.sh configs/animal_2d_keypoint/topdown_heatmap/pigpose/td-hm_hrnet-w32_8xb64-210e_pigpose-256x256.py 2 42
 
 # CHECK YOUR JOB
 # sq
 
 # INTERACTIVE JOBS
-# salloc --time=0:30:0 --ntasks=8 --gres=gpu:a100:1 --mem=16G --nodes=1
+# salloc --time=1:30:0 --ntasks=8 --gres=gpu:p100:4 --mem=64G --nodes=1
+# salloc --time=1:30:0 --ntasks=8 --gres=gpu:v100:2 --mem=64G --nodes=1
 
 # CANCEL JOB
 # scancel <jobid>
