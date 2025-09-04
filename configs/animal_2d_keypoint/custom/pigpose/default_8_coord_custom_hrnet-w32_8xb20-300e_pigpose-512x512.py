73e6b867781e76f76f2ac93bb1a62c29ee244ad9
Francis Ferri <49722697+Francis-Ferri-personal@users.noreply.github.com>
2025-09-04 16:56:10 -0600

    Fix: Update bachsize to 20

--- configs/animal_2d_keypoint/custom/pigpose/default_8_coord_custom_hrnet-w32_8xb20-300e_pigpose-512x512.py
+++ configs/animal_2d_keypoint/custom/pigpose/default_8_coord_custom_hrnet-w32_8xb20-300e_pigpose-512x512.py
@@ -155,7 +155,7 @@ val_pipeline = [
 
 # data loaders
 train_dataloader = dict(
-    batch_size=2,
+    batch_size=20,
     num_workers=2,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
