c0a41792fc9f3cbb2ffa2bd1bac1bc12454b11a8
Francis Ferri <49722697+Francis-Ferri-personal@users.noreply.github.com>
2025-09-08 11:47:00 -0600

    Updating Toch version 2.7

--- tools/drac-train.sh
+++ tools/drac-train.sh
@@ -64,10 +64,10 @@ pip install --upgrade pip
 
 
 # INSTALL DEPENDENCIES
-pip3 install torch torchvision --index-url https://download.pytorch.org/whl/cu126
+pip install torch==2.7.1 torchvision==0.22.1 --index-url https://download.pytorch.org/whl/cu126
 pip install -U openmim
 mim install mmengine
-mim install "mmcv>=2.0.1"
+mim install "mmcv==2.1.0"
 mim install "mmdet>=3.1.0"
 pip install -r requirements.txt
 python setup.py install
