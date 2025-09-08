72ceb713b31100cc1fe53febcad6c45839f5dd43
Francis Ferri <49722697+Francis-Ferri-personal@users.noreply.github.com>
2025-09-08 14:58:20 -0600

    Trying to add compute canada

--- tools/drac-train.sh
+++ tools/drac-train.sh
@@ -64,7 +64,7 @@ pip install --upgrade pip
 
 
 # INSTALL DEPENDENCIES
-pip3 install torch==2.7.1 torchvision==0.22.1 --index-url https://download.pytorch.org/whl/cu126
+pip3 install torch==2.6.0+computecanada torchvision==0.21.0+computecanada
 pip install -U openmim
 mim install mmengine
 mim install "mmcv==2.1.0"
